156ff07a48e3037b7e61980cc3e9bf58520dd315
mohsina-rifa <mohsina.rifa@brainstation-23.com>
2025-10-13 14:00:22 +0600

    Problem ID: 1913/B

---
+++ day-01/1931B/1931B.ts
@@ -0,0 +1,56 @@
+import * as fs from "fs";
+
+let input: string[];
+let currentLine = 0;
+
+const readline = (): string => {
+  return input[currentLine++];
+};
+
+const solve = (): void => {
+  const s = readline();
+
+  const indexOfAllZero: number[] = [];
+  const indexOfAllOne: number[] = [];
+
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === "0") {
+      indexOfAllZero.push(i);
+    } else {
+      indexOfAllOne.push(i);
+    }
+  }
+
+  let t = s.split("");
+
+  const minPairs = Math.min(indexOfAllZero.length, indexOfAllOne.length);
+
+  for (let i = 0; i < minPairs; i++) {
+    t[indexOfAllZero[i]] = "1";
+    t[indexOfAllOne[i]] = "0";
+  }
+
+  let cost = 0;
+
+  for (let i = 0, j = 0; i < t.length; i++) {
+    if (t[i] === s[j]) {
+      cost++;
+    } else {
+      j++;
+    }
+  }
+
+  console.log(cost);
+};
+
+const main = (): void => {
+  input = fs.readFileSync("input.txt", "utf8").trim().split("\n");
+  const testcases: number = parseInt(readline());
+
+  let loop = testcases;
+  while (loop--) {
+    solve();
+  }
+};
+
+main();
