9d44b4f35b94d1ed15abcfc879595dd375937ae0
mohsina-rifa <mohsina.rifa@brainstation-23.com>
2025-11-11 19:49:08 +0600

    Problem ID: 2140/B

---
+++ day-20/2140B/code.ts
@@ -0,0 +1,35 @@
+import * as fs from "fs";
+
+let input: string[];
+let currentLine = 0;
+
+const readline = (): string => {
+  return input[currentLine++];
+};
+
+const solve = (): void => {
+  const x = parseInt(readline());
+  
+  if (x % 2 === 0) {
+    console.log(x / 2);
+  } else {
+    console.log(x * 2);
+  }
+};
+
+const main = (): void => {
+  // input = fs.readFileSync("input.txt", "utf8").trim().split("\n");
+  input = fs
+    .readFileSync(0, "utf8")
+    .trim()
+    .split("\n")
+    .map((line) => line.trim());
+  const testcases: number = parseInt(readline());
+
+  let loop = testcases;
+  while (loop--) {
+    solve();
+  }
+};
+
+main();
