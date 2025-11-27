fc7a1192fed6c273d95854a5899e349853c0e3b8
mohsina-rifa <mohsina.rifa@brainstation-23.com>
2025-11-27 13:56:45 +0600

    Problem ID: 1873/C

---
+++ day-26/1873C/code.ts
@@ -0,0 +1,53 @@
+import * as fs from "fs";
+
+let input: string[];
+let currentLine = 0;
+
+const readline = (): string => {
+  return input[currentLine++];
+};
+
+const solve = (): void => {
+  const ring: number[][] = [
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 2, 2, 2, 2, 2, 2, 2, 2, 1],
+    [1, 2, 3, 3, 3, 3, 3, 3, 2, 1],
+    [1, 2, 3, 4, 4, 4, 4, 3, 2, 1],
+    [1, 2, 3, 4, 5, 5, 4, 3, 2, 1],
+    [1, 2, 3, 4, 5, 5, 4, 3, 2, 1],
+    [1, 2, 3, 4, 4, 4, 4, 3, 2, 1],
+    [1, 2, 3, 3, 3, 3, 3, 3, 2, 1],
+    [1, 2, 2, 2, 2, 2, 2, 2, 2, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+  ];
+
+  let sum = 0;
+  for (let i = 0; i < 10; i++) {
+    const line = readline();
+
+    for (let j = 0; j < 10; j++) {
+      if (line[j] === "X") {
+        sum += ring[i][j];
+      }
+    }
+  }
+
+  console.log(sum);
+};
+
+const main = (): void => {
+  // input = fs.readFileSync("input.txt", "utf8").trim().split("\n");
+  input = fs
+    .readFileSync(0, "utf8")
+    .trim()
+    .split("\n")
+    .map((line) => line.trim());
+  const testcases: number = parseInt(readline());
+
+  let loop = testcases;
+  while (loop--) {
+    solve();
+  }
+};
+
+main();
